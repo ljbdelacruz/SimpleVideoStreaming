6f549399624eb08c92f476f225087dffa21b90a1
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2018-11-15 14:34:18 +0800

    View anime info and its episodes func

--- SimpleVideoStreamingApp/DataModel/Episodes.swift
+++ SimpleVideoStreamingApp/DataModel/Episodes.swift
@@ -7,15 +7,36 @@
 //
 
 import Foundation
+import Firebase
 
 class Episodes{
-    var source,title:String;
+    var id,source,title:String;
     init(){
         self.source="";
         self.title="";
+        self.id="";
     }
-    func set(source:String, title:String){
+    func set(id:String,source:String, title:String){
         self.source=source;
         self.title=title;
+        self.id=id;
+    }
+    
+    //MARK: static methods
+    static func all(animeTitle:String, completionHandler: @escaping (Episodes?, String?) -> ()){
+        let db=Database.database().reference().child("Anime").child(animeTitle).child("episodes");
+        FirebaseCustom.RetrieveDataValue(db: db, completionHandler: {
+            (ds) in
+            guard let dict=ds!.value as! Dictionary<String, Any>? else{
+                completionHandler(nil, "Cannot Fetch data please make sure you are connected to the internet")
+                return
+            }
+            completionHandler(self.Setup(key:ds!.key, dict: dict), nil);
+        })
+    }
+    static func Setup(key:String, dict:[String:Any])->Episodes{
+        let temp=Episodes();
+        temp.set(id:key, source: dict["source"] as! String, title: dict["title"] as! String);
+        return temp;
     }
 }
